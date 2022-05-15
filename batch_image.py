d2bb6d1dca90c6a968bfcec95ac91646a8f7843c
Maksatbek Yrysbaev <80979284+Yrysbaev@users.noreply.github.com>
2022-05-15 12:49:17 +0600

    Update batch_image.py

--- batch_image.py
+++ batch_image.py
@@ -1,10 +1,9 @@
 from PIL import Image, ImageFilter, ImageDraw, ImageFont, ImageColor, ImageEnhance
 
 import os
-os.chdir("Instagram")
 for path,dirc,files in os.walk("."):
     for file in files: 
-        if file.endswith('.jpg') or file.endswith(".png"):
+        if file.endswith('.jpg') or file.endswith(".png") or file.endswith(".jpeg"):
             img = Image.open(file)
             fn, flext = os.path.splitext(file)
             b_w = img.convert("L")
@@ -12,9 +11,9 @@ for path,dirc,files in os.walk("."):
             resize = detail.resize((1080, 1080))
             width, height = resize.size
             draw = ImageDraw.Draw(resize)
-            watermark  = "Sultan Chai"
+            watermark  = "COM21"
             color = "WHITE"
-            font = ImageFont.truetype("/home/umut/Desktop/Wall Notes.otf", 60)
+            font = ImageFont.truetype("Wall Notes.otf", 60)
             textwidth, textheight = draw.textsize(watermark, font)
             edge = 10 
             p_x = width - textwidth - edge
